9058ec5068e3e792e2577d6b8a78b5cdf57037c6
He Yihao <2667004988@qq.com>
2019-10-15 22:34:30 +0800

    add java

--- centos7/hadoop/javainstall.sh
+++ centos7/hadoop/javainstall.sh
@@ -4,5 +4,5 @@ mkdir /usr/local/java/
 tar -zxvf ~/jdk-8u191-linux-x64.tar.gz -C /usr/local/java/
 cat javaHome.txt >> /etc/profile
 source /etc/profile
-ln -s /usr/local/java/jdk1.8.0_171/bin/java /usr/bin/java
+ln -s /usr/local/java/jdk1.8.0_191 /bin/java /usr/bin/java
 java -version
