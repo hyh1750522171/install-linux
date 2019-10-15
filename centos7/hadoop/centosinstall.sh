38eecc5ed60659e5e914360cb72c1c73fabfbf82
He Yihao <2667004988@qq.com>
2019-10-15 22:25:41 +0800

    add hadoop

---
+++ centos7/hadoop/centosinstall.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+yum install -y wget
+wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
+yum clean all
+yum makecache
+yum install -y net-tools 
+yum install -y vim
+yum install -y python3
