4bc9f53bb7e8df3bc5a8b102e6b719a0f126abc6
He Yihao <2667004988@qq.com>
2019-10-16 08:49:56 +0800

    add jieshao

---
+++ centos7/hadoop/main.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+####### 安装hadoop #########
+# 
+######## 介绍结束 ###############
+
+
+########## 脚本开始 ############
+bash centosinstall.sh
+bash java install.sh
+bash hadoopinstall.sh
+########## 脚本结束 ############
