e157b24d47e74202753ec46e366bb0009c3d0a59
He Yihao <2667004988@qq.com>
2020-01-04 15:45:24 +0800

    add docker

---
+++ centos7/docker/dockerjinxiangjiasu.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo mkdir -p /etc/docker
+sudo tee /etc/docker/daemon.json <<-'EOF'
+{
+  "registry-mirrors": ["https://q4a5yqz4.mirror.aliyuncs.com"]
+}
+EOF
+sudo systemctl daemon-reload
+sudo systemctl restart docker
