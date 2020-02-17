36a0ba975808102137a331d7acc40c1b1040a903
He Yihao <2667004988@qq.com>
2020-02-17 10:36:30 +0800

    add

--- Ubuntu/bixuanzhuangderuanjian.sh
+++ Ubuntu/bixuanzhuangderuanjian.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-
+sudo apt-get remove vim-common -y
 sudo apt-get update
-sudo apt-get install -y fish net-tools ranger caca-utils highlight atool w3m poppler-utils mediainfo openssh-client openssh-server curl screenkey simplescreenrecorder
+git clone https://github.com/hyh1750522171/google-chrome.git
+sudo apt-get install -y  typora ghostscript neovim python3-pip vlc fish net-tools ranger caca-utils highlight atool w3m poppler-utils mediainfo openssh-client openssh-server curl screenkey simplescreenrecorder ipython3
 sudo service ssh start
 sudo ps -e | grep ssh
-sudo apt-get remove vim-common -y
 curl -L https://git.oh-my.fish | fish
 curl -L https://git.oh-my.fish
 wget  https://git.oh-my.fish
 curl -L https://git.oh-my.fish | fish
-
+ssh-keygen -t rsa -b 4096 -C "2667004988@qq.com"
